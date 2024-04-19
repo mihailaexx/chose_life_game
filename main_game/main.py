a7e16f0ac60bd6c9f1fae3cbc8e22e870ab17eb9
mihailaexx <m_bulushev@kbtu.kz>
2024-04-19 21:49:25 +0500

    installed all dependencies for the menu

--- main_game/main.py
+++ main_game/main.py
@@ -7,57 +7,83 @@ GAME_MENU, GAME_SETTINGS, GAME_PAUSE, GAME_RUNNING, GAME_OVER = range(5) #* пр
 
 class Menu:
     def __init__(self) -> None:
-        self.bg = "imgs/kbtu.png"
-        self.play_con_button = pygame.Rect(0, 0, 533, 146)
-        self.play_con_button.center = (640, 283)
-        self.settings_button = pygame.Rect(0, 0, 533, 146)
-        self.settings_button.center = (640, 453)
-        self.exit_button = pygame.Rect(0, 0, 533, 146)
-        self.exit_button.center = (640, 623)
-        self.volume_slider = pygame.Rect(0, 0, 826, 20)
-        self.volume_slider.center = (640, 360)
-        self.back_button = pygame.Rect(0, 0, 533, 146)
-        self.back_button.center = (640, 623)
+        self.bg = pygame.transform.scale(pygame.image.load("imgs/kbtu.png"), (1920, 1080))
+        self.play_button = pygame.Rect(0, 0, 800, 220)
+        self.play_button_text = pygame.font.Font(None, 72).render("Start", True, (0, 0, 0))
+        self.play_button.center = (960, 425)
+        self.play_button_text_rect = self.play_button_text.get_rect(center=(960, 425))
+        self.conn_button = pygame.Rect(0, 0, 800, 220)
+        self.conn_button_text = pygame.font.Font(None, 72).render("Continue", True, (0, 0, 0))
+        self.conn_button.center = (960, 425)
+        self.conn_button_text_rect = self.conn_button_text.get_rect(center=(960, 425))
+        self.settings_button = pygame.Rect(0, 0, 800, 220)
+        self.settings_button_text = pygame.font.Font(None, 72).render("Settings", True, (0, 0, 0))
+        self.settings_button.center = (960, 680)
+        self.settings_button_text_rect = self.settings_button_text.get_rect(center=(960, 680))
+        self.exit_button = pygame.Rect(0, 0, 800, 220)
+        self.exit_button_text = pygame.font.Font(None, 72).render("Exit", True, (0, 0, 0))
+        self.exit_button.center = (960, 935)
+        self.exit_button_text_rect = self.exit_button_text.get_rect(center=(960, 935))
+        self.volume_slider = pygame.Rect(0, 0, 1240, 20)
+        self.volume_slider.center = (960, 540)
+        self.back_button = pygame.Rect(0, 0, 800, 220)
+        self.back_button_text = pygame.font.Font(None, 72).render("Back to main menu", True, (0, 0, 0))
+        self.back_button.center = (960, 935)
+        self.back_button_text_rect = self.back_button_text.get_rect(center=(960, 935))
         # звуки
         # self.click_sound = pygame.mixer.Sound("click.wav")
         # self.bg_sound = pygame.mixer.music.load("bg.wav")
     def draw(self, screen, state):
         if state == GAME_MENU: #* в меню отображать 3 кнопки - играть, настройки, выход
-            screen.blit(pygame.image.load(self.bg), (0, 0))
-            pygame.draw.rect(screen, (255, 0, 0), self.play_con_button)
+            screen.blit(self.bg, (0, 0))
+            pygame.draw.rect(screen, (255, 0, 0), self.play_button)
+            screen.blit(self.play_button_text, self.play_button_text_rect)
             pygame.draw.rect(screen, (255, 0, 0), self.settings_button)
+            screen.blit(self.settings_button_text, self.settings_button_text_rect)
             pygame.draw.rect(screen, (255, 0, 0), self.exit_button)
+            screen.blit(self.exit_button_text, self.exit_button_text_rect)
         elif state == GAME_PAUSE: #* в паузе отображать кнопку продолжить, кнопку настройки, кнопку выхода в меню
-            screen.blit(pygame.image.load(self.bg), (0, 0))
-            pygame.draw.rect(screen, (255, 0, 0), self.play_con_button)
+            screen.blit(self.bg, (0, 0))
+            pygame.draw.rect(screen, (255, 0, 0), self.conn_button)
+            screen.blit(self.conn_button_text, self.conn_button_text_rect)
             pygame.draw.rect(screen, (255, 0, 0), self.settings_button)
-            pygame.draw.rect(screen, (255, 0, 0), self.exit_button)
+            screen.blit(self.settings_button_text, self.settings_button_text_rect)
+            pygame.draw.rect(screen, (255, 0, 0), self.back_button)
+            screen.blit(self.back_button_text, self.back_button_text_rect)
         elif state == GAME_SETTINGS: #* в настройках отображать слайдер громкости, кнопку назад
-            screen.blit(pygame.image.load(self.bg), (0, 0))
+            screen.blit(self.bg, (0, 0))
             pygame.draw.rect(screen, (255, 0, 0), self.volume_slider)
             pygame.draw.rect(screen, (255, 0, 0), self.back_button)
+            screen.blit(self.back_button_text, self.back_button_text_rect)
+            self.volume_text = pygame.font.Font(None, 36).render(f"{int(pygame.mixer.music.get_volume()*100)}", True, (255, 0, 0))
+            self.volume_text_rect = self.volume_text.get_rect(center=(960, 580))
+            screen.blit(self.volume_text, self.volume_text_rect)
+            pygame.draw.circle(screen, (0, 0, 0), (pygame.mixer.music.get_volume()*1240+340, 540), 15)
+            pygame.draw.circle(screen, (255, 0, 0), (pygame.mixer.music.get_volume()*1240+340, 540), 10)
         elif state == GAME_OVER:
             pass
 
 class Level:
     def __init__(self) -> None:
-        self.bg = "imgs/independence_hall.png"
-        self.event_bg = pygame.Rect(0, 0, 826, 146)
-        self.event_bg.center = (640, 97)
-        self.pause_button = pygame.Rect(0, 0, 146, 146)
-        self.pause_button.center = (103, 97)
+        self.bg = pygame.image.load("imgs/independence_hall.png")
+        self.event_bg = pygame.Rect(0, 0, 1240, 220)
+        self.event_bg.center = (960, 145)
+        self.pause_button = pygame.Rect(0, 0, 220, 220)
+        self.pause_button.center = (150, 145)
         self.n_of_card = 2 #* количество карточек на экране(2 или 3)
         # self.click_sound = pygame.mixer.Sound("click.wav")
         # self.bg_sound = pygame.mixer.music.load("bg.wav")
     def draw(self, screen, state):
-        screen.blit(pygame.image.load(self.bg), (0, 0))
+        screen.blit(self.bg, (0, 0))
         pygame.draw.rect(screen, (255, 0, 0), self.event_bg)
         pygame.draw.rect(screen, (255, 0, 0), self.pause_button)
-        if self.n_of_card == 2:
-            self.card1 = pygame.Rect(0, 0, 400, 146)
-            self.card1.center = (427, 445)
-            self.card2 = pygame.Rect(0, 0, 400, 146)
-            self.card2.center = (853, 445)
+        if self.n_of_card == 0:
+            pass
+        elif self.n_of_card == 2:
+            self.card1 = pygame.Rect(0, 0, 600, 425)
+            self.card1.center = (640, 667)
+            self.card2 = pygame.Rect(0, 0, 600, 425)
+            self.card2.center = (1280, 667)
             pygame.draw.rect(screen, (255, 0, 0), self.card1)
             pygame.draw.rect(screen, (255, 0, 0), self.card2)
         # elif self.n_of_card == 3:
@@ -74,7 +100,7 @@ class Player:
 
 class Game:
     def __init__(self) -> None:
-        self.screen = pygame.display.set_mode((1280, 720))
+        self.screen = pygame.display.set_mode((1920, 1080))
         self.screen.fill((255, 255, 255))
         self.game_state = GAME_MENU
         self.menu = Menu()
@@ -83,6 +109,7 @@ class Game:
     def reset(self):
         self.player = Player()
         self.level = Level()
+        pygame.mixer.music.set_volume(0.1)
 
     def event_handle(self):
         mouse_pos = pygame.mouse.get_pos()
@@ -91,7 +118,12 @@ class Game:
                 pygame.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
+                    if self.game_state == GAME_RUNNING:
+                        self.game_state = GAME_PAUSE
+                    elif self.game_state == GAME_PAUSE:
+                        self.game_state = GAME_RUNNING
+                    elif self.game_state == GAME_MENU:
+                        pygame.quit()
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if self.game_state == GAME_RUNNING:
                     if self.level.card1.collidepoint(mouse_pos):
@@ -102,38 +134,43 @@ class Game:
                         print("pause")
                         self.game_state = GAME_PAUSE
                 elif self.game_state == GAME_MENU: #* если игра в меню
-                    if self.menu.play_con_button.collidepoint(mouse_pos):
-                        print("play")
+                    if self.menu.conn_button.collidepoint(mouse_pos):
+                        print("conn")
                         self.game_state = GAME_RUNNING
                     elif self.menu.settings_button.collidepoint(mouse_pos):
                         print("settings")
+                        self.prev_game_state = GAME_MENU
                         self.game_state = GAME_SETTINGS
-                    elif self.menu.exit_button.collidepoint(mouse_pos):
-                        print("exit")
+                    elif self.menu.back_button.collidepoint(mouse_pos):
+                        print("back")
                         pygame.quit()
                 elif self.game_state == GAME_SETTINGS: # если игра в настройках
-                    if self.menu.volume_slider.collidepoint(mouse_pos):
-                        print((mouse_pos[0]-227)/826) # звук от 0 до 1 поэтому делим на 826
-                    elif self.menu.back_button.collidepoint(mouse_pos):
-                        self.game_state = GAME_MENU
+                    # if self.menu.volume_slider.collidepoint(mouse_pos):
+                        
+                    if self.menu.back_button.collidepoint(mouse_pos):
+                        self.game_state = self.prev_game_state
                 elif self.game_state == GAME_PAUSE: # если игра на паузе
-                    if self.menu.play_con_button.collidepoint(mouse_pos):
+                    if self.menu.play_button.collidepoint(mouse_pos):
                         print("continue")
                         self.game_state = GAME_RUNNING
                     elif self.menu.settings_button.collidepoint(mouse_pos):
                         print("settings")
+                        self.prev_game_state = GAME_PAUSE
                         self.game_state = GAME_SETTINGS
                     elif self.menu.exit_button.collidepoint(mouse_pos):
                         print("exit")
                         self.game_state = GAME_MENU
-
+        if pygame.mouse.get_pressed()[0]:
+            if self.game_state == GAME_SETTINGS:
+                if self.menu.volume_slider.collidepoint(mouse_pos):
+                    pygame.mixer.music.set_volume((mouse_pos[0]-340)/1240)
     def run(self):
         while True:
-            self.event_handle()
             if self.game_state == GAME_RUNNING:
                 self.level.draw(self.screen, self.game_state)
             else:
                 self.menu.draw(self.screen, self.game_state)
+            self.event_handle()
             pygame.display.flip()
             pygame.time.Clock().tick(60)
     
